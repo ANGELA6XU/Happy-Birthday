d3bd1135fff765d077615f6f40be3966896d1a11
ANGELA6XU <jx255@cornell.edu>
2018-07-29 22:53:15 +0800

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,479 @@
+# Happy Birthday
+import os
+import turtle
+import time
+import random
+
+
+# resize the window
+turtle.setup(width = 1450, height = 800, startx = 0, starty = 0)
+
+# play music
+os.system("afplay bday_song.mp3&")
+
+
+# create graph 
+
+my_turtle = turtle.Turtle()
+my_turtle.screen.title("Happy Birthday to lovely JuJu Zi ^_^ ^_^ ")
+
+my_turtle.width(14)
+my_turtle.color("salmon")
+new = turtle.getscreen()
+my_turtle.speed(3.5)
+
+
+new.bgcolor("ivory")
+new.bgpic("cake.gif")
+#new.bgpic("cake.gif")
+#new.bgpic("gif.gif")
+
+
+# Hidden Work(penup)
+my_turtle.left(180)
+my_turtle.penup()
+my_turtle.forward(300)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.pendown()
+
+# Printing H
+
+
+#start to draw
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+
+my_turtle.penup()
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.pendown()
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(50)
+
+
+# printing A
+
+my_turtle.penup()
+my_turtle.left(90)
+my_turtle.forward(15)
+my_turtle.pendown()
+my_turtle.left(70)
+my_turtle.forward(110)
+my_turtle.right(70)
+my_turtle.right(70)
+my_turtle.forward(110)
+my_turtle.left(180)
+my_turtle.forward(55)
+my_turtle.left(70)
+my_turtle.forward(38)
+my_turtle.left(70)
+my_turtle.penup()
+my_turtle.forward(55)
+my_turtle.left(110)
+
+my_turtle.forward(100)
+
+# printing P
+
+my_turtle.left(90)
+my_turtle.pendown()
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.right(20)
+my_turtle.forward(20)
+my_turtle.right(70)
+my_turtle.forward(40)
+my_turtle.right(70)
+my_turtle.forward(20)
+my_turtle.right(20)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.penup()
+my_turtle.forward(100)
+
+
+# printing P
+
+my_turtle.left(90)
+my_turtle.pendown()
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.right(20)
+my_turtle.forward(20)
+my_turtle.right(70)
+my_turtle.forward(40)
+my_turtle.right(70)
+my_turtle.forward(20)
+my_turtle.right(20)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.penup()
+my_turtle.forward(100)
+
+# printing Y
+
+my_turtle.forward(20)
+my_turtle.pendown()
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(30)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.right(60)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.left(30)
+
+# go to Home
+
+my_turtle.penup()
+my_turtle.home()
+
+my_turtle.color("light pink")
+new.bgcolor("honeydew")
+#new.bgpic("gif.gif")
+#new.bgpic("box.gif")
+new.bgpic("group.gif")
+
+# setting second row
+
+my_turtle.backward(300)
+my_turtle.right(90)
+my_turtle.forward(60)
+my_turtle.left(180)
+
+
+# printing P
+
+
+my_turtle.pendown()
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.right(20)
+my_turtle.forward(20)
+my_turtle.right(70)
+my_turtle.forward(40)
+my_turtle.right(70)
+my_turtle.forward(20)
+my_turtle.right(20)
+my_turtle.forward(50)
+my_turtle.backward(50)
+my_turtle.left(180)
+my_turtle.right(20)
+my_turtle.forward(20)
+my_turtle.right(70)
+my_turtle.forward(40)
+my_turtle.right(70)
+my_turtle.forward(20)
+my_turtle.right(20)
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(10)
+
+
+# go to Home
+
+my_turtle.penup()
+my_turtle.home()
+
+# setting up
+
+my_turtle.backward(200)
+my_turtle.right(90)
+my_turtle.forward(10)
+my_turtle.left(90)
+my_turtle.pendown()
+my_turtle.forward(20)
+my_turtle.penup()
+my_turtle.home()
+
+# D
+
+my_turtle.backward(150)
+my_turtle.right(90)
+my_turtle.forward(60)
+my_turtle.pendown()
+my_turtle.backward(100)
+my_turtle.right(90)
+my_turtle.forward(10)
+my_turtle.backward(70)
+my_turtle.left(180)
+my_turtle.right(20)
+my_turtle.forward(20)
+my_turtle.right(70)
+my_turtle.forward(88)
+my_turtle.right(70)
+my_turtle.forward(20)
+my_turtle.right(20)
+my_turtle.forward(70)
+
+my_turtle.penup()
+my_turtle.home()
+
+# set up for A
+
+my_turtle.backward(50)
+my_turtle.right(90)
+my_turtle.forward(65)
+my_turtle.left(90)
+
+
+
+# printing A
+
+
+my_turtle.pendown()
+my_turtle.left(70)
+my_turtle.forward(110)
+my_turtle.right(70)
+my_turtle.right(70)
+my_turtle.forward(110)
+my_turtle.left(180)
+my_turtle.forward(55)
+my_turtle.left(70)
+my_turtle.forward(38)
+my_turtle.left(70)
+my_turtle.penup()
+my_turtle.forward(55)
+my_turtle.left(110)
+
+my_turtle.forward(100)
+
+# printing Y
+
+
+# printing Y
+
+
+my_turtle.pendown()
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(30)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.right(60)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.left(30)
+
+# go to Home 
+
+my_turtle.penup()
+my_turtle.home()
+
+
+# settig position
+
+my_turtle.backward(100)
+my_turtle.right(90)
+my_turtle.forward(215)
+my_turtle.right(90)
+my_turtle.forward(200)
+my_turtle.right(90)
+
+#color
+
+my_turtle.color("gold")
+new.bgcolor("azure")
+#new.bgpic("group.gif")
+#new.bgpic("box.gif")
+new.bgpic("single.gif")
+
+
+# print LuoYun
+
+# print L
+
+my_turtle.pendown()
+my_turtle.forward(100)
+my_turtle.penup()
+my_turtle.backward(100)
+my_turtle.pendown()
+my_turtle.right(90)
+my_turtle.forward(70)
+my_turtle.penup()
+
+# print u
+
+my_turtle.forward(20)
+my_turtle.pendown()
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.penup()
+my_turtle.backward(50)
+my_turtle.pendown()
+my_turtle.right(110)
+my_turtle.forward(15)
+my_turtle.left(20)
+my_turtle.forward(15)
+my_turtle.left(20)
+my_turtle.forward(15)
+my_turtle.left(70)
+my_turtle.forward(50)
+my_turtle.penup()
+
+
+# print o
+
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.backward(55)
+my_turtle.right(90)
+my_turtle.pendown()
+my_turtle.circle(25)
+my_turtle.penup()
+
+
+# print Y
+
+my_turtle.forward(65)
+my_turtle.left(90)
+my_turtle.pendown()
+my_turtle.forward(50)
+my_turtle.left(30)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.right(60)
+my_turtle.forward(60)
+my_turtle.backward(60)
+my_turtle.left(30)
+my_turtle.penup()
+
+
+
+# print u
+
+my_turtle.backward(50)
+my_turtle.right(90)
+my_turtle.forward(40)
+my_turtle.pendown()
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.penup()
+my_turtle.backward(50)
+my_turtle.pendown()
+my_turtle.right(110)
+my_turtle.forward(15)
+my_turtle.left(20)
+my_turtle.forward(15)
+my_turtle.left(20)
+my_turtle.forward(15)
+my_turtle.left(70)
+my_turtle.forward(50)
+my_turtle.penup()
+
+
+# print n
+
+my_turtle.right(90)
+my_turtle.forward(30)
+my_turtle.right(90)
+my_turtle.pendown()
+my_turtle.forward(50)
+my_turtle.backward(50)
+my_turtle.left(110)
+my_turtle.forward(15)
+my_turtle.right(20)
+my_turtle.forward(15)
+my_turtle.right(20)
+my_turtle.forward(15)
+my_turtle.right(70)
+my_turtle.forward(50)
+my_turtle.penup()
+
+
+# design cake
+
+my_turtle.home()
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(200)
+my_turtle.pendown()
+#my_turtle.color("hotpink")
+my_turtle.width(8)
+my_turtle.speed(1.75)
+
+def line(x1, y1, x2, y2, color, pensize):
+  my_turtle.penup()
+  my_turtle.goto(x1, y1)
+  my_turtle.color(color)
+  my_turtle.pensize(pensize)
+  my_turtle.pendown()
+  my_turtle.goto(x2, y2)
+  my_turtle.penup()
+  
+snow_colors = ["hotpink", "silver", "cyan", "green yellow", "light blue",  "purple", "lightcoral", "magenta", 'spring green', 'orange', 'gold', 'chocolate', 'red']
+
+#line(200, 0, 500, 0, random.choice(snow_colors), 6)
+#line(230, 30, 470, 30, random.choice(snow_colors), 6)
+#line(260, 60, 440, 60, random.choice(snow_colors), 6)
+
+#line(200, 0, 500, 0, "orange", 6)
+#line(230, 30, 470, 30, "sky blue", 6)
+#line(260, 60, 440, 60, "dark turquoise", 6)
+
+line(200, -100, 500, -100, "orange", 12)
+line(230, -70, 470, -70, "sky blue", 12)
+line(260, -40, 440, -40, "dark turquoise", 12)
+
+# draw cake
+#my_turtle.goto(280,70)
+my_turtle.goto(280,-30)
+my_turtle.begin_fill()
+my_turtle.pendown()
+my_turtle.color("yellow")
+#my_turtle.goto(420,70)
+my_turtle.goto(420,-30)
+my_turtle.left(90)
+my_turtle.forward(80)
+my_turtle.left(90)
+my_turtle.forward(140)
+my_turtle.left(90)
+my_turtle.forward(80)
+my_turtle.penup()
+my_turtle.end_fill()
+
+#draw candles
+
+def candle(x, color):
+  #my_turtle.goto(x, 135)
+  my_turtle.goto(x, 55)
+  my_turtle.color(color)
+  my_turtle.pendown()
+  my_turtle.pensize(8)
+  #my_turtle.goto(x, 170)
+  my_turtle.goto(x, 88)
+  my_turtle.penup()
+
+candle(290, random.choice(snow_colors))
+candle(310, random.choice(snow_colors))
+candle(330, random.choice(snow_colors))
+candle(350, random.choice(snow_colors))
+candle(370, random.choice(snow_colors))
+candle(390, random.choice(snow_colors))
+candle(410, random.choice(snow_colors))
+
+
+# send the turtle to the corner
+my_turtle.penup()
+my_turtle.goto(-250, 250)
+
+
+turtle.mainloop()
